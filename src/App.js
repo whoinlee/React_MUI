e2ca4bd4341d4ca82d783ed3aa158a7a3f1d8596
WhoIN Lee <whoinlee@gmail.com>
2023-09-11 13:07:00 -0400

    Container, Typography, Theme, and ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import Button from '@mui/material/Button';
+import { Container, Typography } from '@mui/material';
 import SaveIcon from '@mui/icons-material/Save';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Container sx={{height: '100vh'}}>
+      <Typography 
+        variant="h1"
+        sx={{my:4, textAlign: "center", color: "primary.main"} }
+      >
+        Learning Material UI
+      </Typography>
         <Button variant="contained" size="large" color="secondary" startIcon={<SaveIcon />}>
           Save Button
         </Button>
-      </header>
-    </div>
+    </Container>
   );
 }
 
