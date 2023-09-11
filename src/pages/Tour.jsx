93a41a26abcd10aa23da270867e6b9bafdc6bc1a
WhoIN Lee <whoinlee@gmail.com>
2023-09-11 19:08:33 -0400

    finished FAQ section

--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import QuiltedImageList from '../components/ImageLists'
+import ControlledAccordions from "../components/Accordion";
 
 const Tour = () => {
   return (
@@ -18,6 +19,24 @@ const Tour = () => {
             />
             <QuiltedImageList />
         </Box>
+        <Typography variant="h6" component="h4" marginTop={3}>
+            About this ticket
+        </Typography>
+        <Box sx={{ display: "flex" }}>
+            <Typography variant="paragraph" component="p" marginY={3}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit
+            voluptates rem quos delectus debitis earum modi, ipsum veritatis.
+            Perferendis officiis nobis, aut hic praesentium nulla vero, possimus
+            omnis reprehenderit blanditiis quis incidunt minima voluptatibus illum
+            quam corporis libero fugiat doloremque. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Exercitationem officiis commodi beatae
+            animi incidunt necessitatibus aut provident ad ex. Saepe!
+            </Typography>
+        </Box>
+        <Typography variant="h6" component="h4" marginBottom={3}>
+            Frequently Asked Questions
+        </Typography>
+        <ControlledAccordions />
     </Container>
   )
 }
