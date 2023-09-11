78299fb85cf38688c2e1467a8c506ad7600797ac
WhoIN Lee <whoinlee@gmail.com>
2023-09-11 15:36:25 -0400

    ThemeProvider used

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -7,57 +7,84 @@ import Typography from '@mui/material/Typography';
 import {
   AccessTime
 } from '@mui/icons-material';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2"
+          },
+          style: {
+            fontSize: 11
+          }
+        },
+        {
+          props: {
+            variant: "body3"
+          },
+          style: {
+            fontSize: 9
+          }
+        }
+      ]
+    }
+  }
+})
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img className="tourcard-img" alt="img1" src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" components="h2">
-            title goes here
-          </Typography>
-          <Box sx={{
-            display: 'flex',
-            alignItems: 'center'
-          }}>
-            <AccessTime sx={{
-              width: 12.5
-            }} />
-            <Typography variant="body2" components="p" marginLeft={0.5}>
-              5 hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img className="tourcard-img" alt="img1" src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" components="h2">
+              title goes here
             </Typography>
+            <Box sx={{
+              display: 'flex',
+              alignItems: 'center'
+            }}>
+              <AccessTime sx={{
+                width: 12.5
+              }} />
+              <Typography variant="body2" components="p" marginLeft={0.5}>
+                5 hours
+              </Typography>
+            </Box>
+            <Box  sx={{
+                    display: 'flex',
+                    alignItems: 'center'
+                  }}
+                  marginTop={3}>
+              <Rating name="read-only" 
+                      value={4.5}
+                      precision={0.5}
+                      size="small"
+                      readOnly
+              />
+              <Typography variant="body2" components="p" marginLeft={0.5}>
+                4.5
+              </Typography>
+              <Typography variant="body2" components="p" marginLeft={1.5}>
+                (650 reviews)
+              </Typography>
+            </Box>
+            <Box  sx={{
+                    display: 'flex',
+                    alignItems: 'center'
+                  }}
+                  marginTop={3}>
+              <Typography variant="h6" components="h3" marginTop={0}>
+                From C $145
+              </Typography>
+            </Box>
           </Box>
-          <Box  sx={{
-                  display: 'flex',
-                  alignItems: 'center'
-                }}
-                marginTop={3}>
-            <Rating name="read-only" 
-                    value={4.5}
-                    precision={0.5}
-                    size="small"
-                    readOnly
-            />
-            <Typography variant="body2" components="p" marginLeft={0.5}>
-              4.5
-            </Typography>
-            <Typography variant="body2" components="p" marginLeft={1.5}>
-              (650 reviews)
-            </Typography>
-          </Box>
-          <Box  sx={{
-                  display: 'flex',
-                  alignItems: 'center'
-                }}
-                marginTop={3}>
-            <Typography variant="h6" components="h3" marginTop={0}>
-              From C $145
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   )
 }
